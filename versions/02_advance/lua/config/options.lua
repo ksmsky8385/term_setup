97c7cd84b47af3b0b922f57605ffe83b74bb8be9
SM_Kang <ksmsky8385@naver.com>
2026-06-11 00:22:00 +0900

    add_folding

--- versions/02_advance/lua/config/options.lua
+++ versions/02_advance/lua/config/options.lua
@@ -14,3 +14,9 @@ vim.opt.expandtab = false
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:block,a:blinkwait700-blinkoff400-blinkon250"
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
