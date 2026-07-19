0506900b06a03ef95b7ec5092ec2e83f9d08cc81
SM_Kang <ksmsky8385@naver.com>
2026-07-19 19:18:10 +0900

    fix: telescope version down

--- versions/01_default/init.lua
+++ versions/01_default/init.lua
@@ -137,6 +137,7 @@ require("lazy").setup({
     },
     {
         "nvim-telescope/telescope.nvim",
+        version = "0.1.8",
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
